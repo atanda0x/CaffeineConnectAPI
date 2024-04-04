df4f4d88832da5b8d8de7e701c3ed364bed5d35a
atanda0x <atandanafiu21@gmail.com>
2024-04-04 09:15:04 -0700

    added docs

--- handlers/product.go
+++ handlers/product.go
@@ -11,20 +11,32 @@ type Products struct {
 	l *log.Logger
 }
 
+// NewProducts creates a products handler with the given logger
 func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
+// ServeHTTP is the main entry point for the handler and satisfies the http.handler interface
 func (p *Products) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// handle the request for a list of product
 	if r.Method == http.MethodGet {
 		p.getProducts(w, r)
 		return
 	}
+
+	// catch all
+	// if no method is satisfied return err
 	w.WriteHeader(http.StatusMethodNotAllowed)
 }
 
+// getProduct return the products from the datastore
 func (p *Products) getProducts(w http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle GET product")
+
+	// fetch the produc t from datastore
 	lp := data.GetProucts()
+
+	// serialize the list to JSON
 	err := lp.ToJOSN(w)
 	if err != nil {
 		http.Error(w, "Unable to marshal json", http.StatusInternalServerError)
